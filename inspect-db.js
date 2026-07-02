0d21e9dd228376adc168905cf09d1a9d6bcbcb5e
ubairrr <hancock.um@gmail.com>
2026-07-02 20:30:06 +0530

    chore: auto-sync working tree

--- inspect-db.js
+++ inspect-db.js
@@ -3,6 +3,8 @@ const Database = require('better-sqlite3-multiple-ciphers')
 const fs = require('fs')
 const path = require('path')
 
+app.setName('meeting-assist')
+
 app.whenReady().then(() => {
   const keyFile = path.join(app.getPath('userData'), '.meetingassist.key')
   const encrypted = fs.readFileSync(keyFile)
