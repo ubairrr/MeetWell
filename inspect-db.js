c6d81280255e5e6b0cb8f7197c21f0ca1461e535
ubairrr <hancock.um@gmail.com>
2026-07-02 20:25:37 +0530

    chore: auto-sync working tree

---
+++ inspect-db.js
@@ -0,0 +1,23 @@
+const { app, safeStorage } = require('electron')
+const Database = require('better-sqlite3-multiple-ciphers')
+const fs = require('fs')
+const path = require('path')
+
+app.whenReady().then(() => {
+  const keyFile = path.join(app.getPath('userData'), '.meetingassist.key')
+  const encrypted = fs.readFileSync(keyFile)
+  const plainKey = safeStorage.decryptString(encrypted)
+
+  const dbPath = path.join(app.getPath('userData'), 'meetingassist.db')
+  const db = new Database(dbPath, { readonly: true })
+  db.pragma(`key = '${plainKey}'`)
+
+  const rows = db.prepare(
+    'SELECT id, title, meeting_type, started_at, created_at FROM meetings ORDER BY created_at DESC LIMIT 10'
+  ).all()
+
+  console.log(JSON.stringify(rows, null, 2))
+
+  db.close()
+  app.quit()
+})
